8b14495796b39be41d3a5169841cb6481a52fd05
Kirill Rasuli <kirill.rasuli@south.rt.ru>
2024-01-21 11:08:31 +0300

    Fix styling and more

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,40 +2,42 @@ package sqlite
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
-	
-	_ "github.com/mattn/go-sqlite3"
+	"url-shortener/internal/storage"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 type Storage struct {
 	db *sql.DB
 }
 
-func New(storagePath string) (*Storage, error){
+func New(storagePath string) (*Storage, error) {
 	const op = "storage.sqlite.New"
-	
+
 	db, err := sql.Open("sqlite3", storagePath)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	
+
 	stmt, err := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS url(
 			id INTEGER PRIMARY KEY,
 			alias TEXT NOT NULL UNIQUE,
-			url TEXT NOT NULL,
+			url TEXT NOT NULL
 		);
 		CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
 	`)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	
+
 	_, err = stmt.Exec()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	
+
 	return &Storage{db: db}, nil
 }
 
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,6 @@ package storage
 import "errors"
 
 var (
-	ErrUrlNotFound = errors.New("url not found")
+	ErrURLNotFound = errors.New("url not found")
 	ErrURLExists = errors.New("url exists")
 )
